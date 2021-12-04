836ba8fd22d7001fc139a045b26b5d966f339cc9
Nihat-Heyderov-dea-5766 <86143636+Nihat-Heyderov-dea-5766@users.noreply.github.com>
2021-12-05 02:57:09 +0400

    Proyekt uğurla işəsalındı

---
+++ comp-shop-Nihat-Heyderov/src/main/java/az/developia/compshopNihatHeyderov/CompShopNihatHeyderovApplication.java
@@ -0,0 +1,13 @@
+package az.developia.compshopNihatHeyderov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompShopNihatHeyderovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompShopNihatHeyderovApplication.class, args);
+	}
+
+}
